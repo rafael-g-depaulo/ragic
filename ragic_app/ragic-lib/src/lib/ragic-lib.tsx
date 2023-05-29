48e05e984acf4abc06b489cd10e10686f786b8aa
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-05-29 19:27:47 +0000

    Sample change in lib

--- ragic_app/ragic-lib/src/lib/ragic-lib.tsx
+++ ragic_app/ragic-lib/src/lib/ragic-lib.tsx
@@ -1,11 +1,9 @@
-import styles from './ragic-lib.module.css';
-
 /* eslint-disable-next-line */
 export interface RagicLibProps {}
 
 export function RagicLib(props: RagicLibProps) {
   return (
-    <div className={styles['container']}>
+    <div>
       <h1>Welcome to RagicLib!</h1>
     </div>
   );
