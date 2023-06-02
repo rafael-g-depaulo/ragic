bf8aa64092adf1cd27bb7365a344b0863a6bf502
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-06-02 10:17:01 -0300

    Add link routes

--- ragic-lib/src/lib/types.d.ts
+++ ragic-lib/src/lib/types.d.ts
@@ -1,9 +1,21 @@
-export interface Route<routeName extends string> {
+interface BaseRoute<routeName extends string> {
   readonly name: routeName;
   children?: Route[];
   aliases?: string[];
-  component?: React.Component;
 }
+
+type Component = React.ReactElement;
+
+type ConcreteRoute<routeName extends string> = BaseRoute<routeName> & {
+  component: Component;
+};
+type LinkRoute<routeName extends string> = BaseRoute<routeName> & {
+  linkTo: string;
+};
+
+export type Route<routeName extends string> =
+  | ConcreteRoute<routeName>
+  | LinkRoute<routeName>;
 
 export type Routes<names extends string> = { [name in names]: Route<name> };
 
