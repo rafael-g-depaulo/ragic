2264762bc9e331b2f8a4bf44bb1bfb04508691b9
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-06-15 18:41:04 +0000

    implementa funcao base do router.tsx

--- ragic-lib/src/lib/Router.tsx
+++ ragic-lib/src/lib/Router.tsx
@@ -1,15 +1,22 @@
-import React, { PropsWithChildren } from 'react';
-import { Routes } from './types';
+import React, { FC } from 'react';
+import { ConcreteRoute, Routes } from './types';
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom';
 
 export interface RouterProps {
   routes: Routes<string>;
 }
 
-export const Router = <
-  routeNames extends string extends routeNames ? never : string
->(
-  props: RouterProps<routeNames>
-) => {
-  // TODO: actually implement routes
-  return <div>routes here</div>;
+export const Router: FC<RouterProps> = ({ routes}) => {
+  const routePath = []
+  
+  for (const item in routes) {
+    routePath.push({
+      path: "/" + routes[item].name,
+      element: (routes[item] as ConcreteRoute<typeof item>).component
+    })
+  }
+  const routes_ = createHashRouter(routePath)
+  return (
+    <RouterProvider router = {routes_} />
+  )
 };
