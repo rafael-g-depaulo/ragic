bee08c6ff9d243f41df0c1c47dd03803a634b081
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-06-15 16:54:43 +0000

    Add test to createRoutes

---
+++ ragic-lib/src/lib/createRoutes.test.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createRoutes } from './createRoutes';
+
+describe('createRoutes', () => {
+  it('creates a dictionary with the correct structure', () => {
+    const routes = createRoutes([
+      { name: 'home', component: {} as any },
+
+      {
+        name: 'blog',
+        component: {} as any,
+        children: [
+          {
+            name: 'blogpost',
+            component: {} as any,
+          },
+          {
+            name: 'bloglist',
+            component: {} as any,
+          }
+        ],
+      },
+    ]);
+
+    expect(routes).toMatchObject({
+      "name": { name: "name", component: {}},
+      "blog": { name: "blog", component: {}},
+      "blog/blogpost": { name: "blog/blogpost", component: {}},
+      "blog/bloglist": { name: "blog/bloglist", component: {}},
+    })
+  });
+});
