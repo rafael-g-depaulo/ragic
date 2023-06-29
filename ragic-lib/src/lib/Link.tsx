5c1c352091d92c1ff3b1adfbbbaa82c632506aa6
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-06-29 16:50:40 -0300

    Add simple Link implementation

--- ragic-lib/src/lib/Link.tsx
+++ ragic-lib/src/lib/Link.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { Routes } from './types';
+import React, { PropsWithChildren } from 'react';
+import { Link as LinkReactRouterDom } from 'react-router-dom';
 
-interface LinkProps<routeNames extends string> {
+interface LinkProps<
+  routeNames extends string extends routeNames ? never : string
+> extends PropsWithChildren {
   to: routeNames;
 }
 
-export const Link =
-  <routeNames extends string>(routes: Routes<routeNames>) =>
-  ({ to }: LinkProps<routeNames>) => {
-    return <>Linking to {to}</>;
-  };
+export const Link = <
+  routeNames extends string extends routeNames ? never : string
+>({
+  to,
+  children,
+}: LinkProps<routeNames>) => {
+  return <LinkReactRouterDom to={to}>{children}</LinkReactRouterDom>;
+};
