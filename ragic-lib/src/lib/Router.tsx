eefa1299f2876956d90b4b6561a2b2a71104d71b
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-06-29 16:50:40 -0300

    Add simple Link implementation

--- ragic-lib/src/lib/Router.tsx
+++ ragic-lib/src/lib/Router.tsx
@@ -1,11 +1,17 @@
-import React, { FC } from 'react';
+import React, { PropsWithChildren } from 'react';
 import { Routes } from './types';
 
-export interface RouterProps {
-  routes: Routes;
+export interface RouterProps<
+  routeNames extends string extends routeNames ? never : string
+> extends PropsWithChildren {
+  routes: Routes<routeNames>;
 }
 
-export const Router: FC<RouterProps> = (props) => {
+export const Router = <
+  routeNames extends string extends routeNames ? never : string
+>(
+  props: RouterProps<routeNames>
+) => {
   // TODO: actually implement routes
   return <div>routes here</div>;
 };
