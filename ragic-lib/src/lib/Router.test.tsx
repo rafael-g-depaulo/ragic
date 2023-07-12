277e921fab152d75a2c88ff740d1c66a8bfed6d0
lucas <lucasvini1298@gmail.com>
2023-07-11 22:09:21 -0300

    Adiciona teste passável para Router

--- ragic-lib/src/lib/Router.test.tsx
+++ ragic-lib/src/lib/Router.test.tsx
@@ -1,10 +1,12 @@
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/react'
 import { Router } from './Router'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/react'
 
 test ('receives an object with name of pages and returns components', async () => {
 
   render(<Router routes={{ 
     "home": {name: "home", component: <h1> Home </h1>} , "blog/list": {name: "bloglist", component: <h1> List </h1> }, "blog/post": {name: "blogpost", component: <h1> Post </h1>}
   }} />)
+
+  
 })
