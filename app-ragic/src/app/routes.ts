f43af960bc5e8186e437de876fcaf0f6c2d1c194
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-09-13 20:29:30 -0300

    Finish implementing example app

--- app-ragic/src/app/routes.ts
+++ app-ragic/src/app/routes.ts
@@ -2,9 +2,14 @@ import { UseRoutes, createRoutes, path } from '@ragic/ragic-lib';
 
 import Home from './pages/Home';
 import BlogList from './pages/BlogList';
+import BlogPostView from './pages/BlogPostView';
 
 const routes = createRoutes()
-  .path('/', { component: Home })
-  .path('/blog', { children: path('/', { component: BlogList }) });
+	.path('/', { component: Home })
+	.path('/blog', {
+		children: path('/', { component: BlogList }).path('/:blog_id', {
+			component: BlogPostView,
+		}),
+	});
 
 export const { Link, Router, useRouteParams } = UseRoutes(routes);
